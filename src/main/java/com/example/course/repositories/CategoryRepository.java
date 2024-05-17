4e6f80d37e78503d7c86b16a1cf5f56f7b37874e
Raphael Monte <raphael.monte@icloud.com>
2024-05-17 17:26:16 -0300

    Category

---
+++ src/main/java/com/example/course/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.example.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.course.entites.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+	
+}
